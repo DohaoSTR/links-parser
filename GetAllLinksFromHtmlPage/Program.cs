46da664a93a10f2ca61dd471ba50f1f51e815e6b
DohaoSTR <53302920+DohaoSTR@users.noreply.github.com>
2020-07-23 19:39:41 +0300

    Version 1.1.

--- GetAllLinksFromHtmlPage/Program.cs
+++ GetAllLinksFromHtmlPage/Program.cs
@@ -5,68 +5,97 @@ using System.Text.RegularExpressions;
 
 namespace GetAllLinksFromHtmlPage
 {
-    class Program
+    public class Program
     {
-        static Dictionary<string, string> SplitParams(string pstr)
+        private static readonly OutMethod writeLine = Console.WriteLine;
+        private static readonly InMethod readLine = Console.ReadLine;
+
+        private delegate void OutMethod(string str);
+        private delegate string InMethod();
+
+        public static void Main()
         {
-            Dictionary<string, string> res = new Dictionary<string, string>();
-            if (string.IsNullOrWhiteSpace(pstr)) return res;
-            string[] ps = pstr.Split('&');
+            string url = SplitParams(readLine())["urlInput"];
+
+            url = Uri.UnescapeDataString(url);
+
+            writeLine("Content-Type: text/html\n\n");
+            writeLine("<html>\n<head>");
+            writeLine("\t<title>LAB_CGI</title>");
+            writeLine("</head>\n");
+            writeLine("<body>\n");
+            writeLine("<p>Все ссылки располагающиеся на указанном сайте: " + url + "</p>\n"); ;
+            writeLine("<ol>\n");
+
+            string html = GetHtmlCode(url);
+
+            foreach (string link in GetAllLinks(html, url))
+            {
+                writeLine("<li>\n<a href = \"" + link + "\">\n" + link + "</a>\n</li>\n");
+            }
+
+            writeLine("</ol>\n");
+            writeLine("</body>\n</html>");
+        }
+
+        private static Dictionary<string, string> SplitParams(string stringWithParams)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            if (string.IsNullOrWhiteSpace(stringWithParams))
+            {
+                return result;
+            }
+
+            string[] ps = stringWithParams.Split('&');
             string[] a;
+
             foreach (string p in ps)
             {
                 a = p.Split('=');
-                res.Add(a[0], a[1]);
+                result.Add(a[0], a[1]);
             }
-            return res;
+
+            return result;
         }
 
-        delegate void OutMethod(string str);
-        delegate string InMethod();
-
-        static OutMethod wl = Console.WriteLine;
-        static InMethod rl = Console.ReadLine;
-
-        static void Main()
+        private static string GetHtmlCode(string url)
         {
-            string url = SplitParams(rl())["urlInput"];
-            url = Uri.UnescapeDataString(url);
-            wl("Content-Type: text/html\n\n");
-            wl("<html>\n<head>");
-            wl("\t<title>LAB_CGI</title>");
-            wl("</head>\n");
-            wl("<body>\n");
-            wl("<p>Все ссылки располагающиеся на указанном сайте: " + url + "</p>\n"); ;
-            wl("<ol>\n");
-            string html = GetHtmlCode(url);
-            foreach (string link in GetAllLinks(html, url))
-                wl("<li>\n<a href = \"" + link + "\">\n" + link + "</a>\n</li>\n");
-            wl("</ol>\n");
-            wl("</body>\n</html>");
-        }
+            WebClient client = new WebClient
+            {
+                Credentials = CredentialCache.DefaultNetworkCredentials
+            };
 
-        public static string GetHtmlCode(string url)
-        {
-            WebClient client = new WebClient();
-            client.Credentials = CredentialCache.DefaultNetworkCredentials;
             return client.DownloadString(url);
         }
 
-        public static List<string> GetAllLinks(string html, string url)
+        private static List<string> GetAllLinks(string html, string url)
         {
             List<string> listLinks = new List<string>();
+
             Regex regexForAllLinks = new Regex("(?: href| src)=[\"|']?(.*?)[\"|'|>]+");
+
             Regex regexForLinksWithoutProtocol = new Regex("^/");
+
             if (regexForAllLinks.IsMatch(html))
             {
                 foreach (Match matchAllLinks in regexForAllLinks.Matches(html))
                 {
                     if (regexForLinksWithoutProtocol.IsMatch(matchAllLinks.Groups[1].Value))
+                    {
                         listLinks.Add(url + matchAllLinks.Groups[1].Value);
+                    }
                     else
+                    {
                         listLinks.Add(matchAllLinks.Groups[1].Value);
+                    }
                 }
             }
+            else
+            {
+                throw new Exception("На html странице ссылок не найдено!");
+            }
+
             return listLinks;
         }
     }
